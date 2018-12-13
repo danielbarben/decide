d3c13b8cfeece604484cceb6015e2267a0e244f5
Daniel Barben <daniel.barben@tamedia.ch>
2018-12-14 00:16:51 +0100

    AllowOrigin

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = require('./api/routes.js');
 const bodyParser = require('body-parser');
-//const cors = require("cors")
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+//app.use(cors(corsOptions))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(bodyParser.urlencoded({
   extended: true 
