825357a73b76fd9fe1326da75605147e3d6eeee5
Daniel Barben <daniel.barben@tamedia.ch>
2018-12-15 11:53:48 +0100

    Mail

--- frontend/src/Landingpage/Footer.js
+++ frontend/src/Landingpage/Footer.js
@@ -4,7 +4,7 @@ import './landingpage.css';
 class Footer extends Component {
 
   render() {
-    const content = <div className='footer'><hr/>Programmierung: <a href="daniel.barben@tamedia.ch">Daniel Barben</a> / Tamedia 2018</div>
+    const content = <div className='footer'><hr/>Programmierung: <a href="mailto:daniel.barben@tamedia.ch">Daniel Barben</a> / Tamedia 2018</div>
     return <div>{content}</div>
   }
 }
