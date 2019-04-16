7976651d8f4dab0b7016287414f50811ded31509
Daniel Barben <daniel.barben@tamedia.ch>
2019-04-16 17:21:03 +0200

    Allow-Header

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = require('./api/routes.js');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
