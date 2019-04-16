1dcca8debd0e11c687f678f58e9ae95ed63aaf2f
Daniel Barben <daniel.barben@tamedia.ch>
2019-04-16 10:32:47 +0200

    deleted

--- backend/db/update.js
+++ backend/db/update.js
@@ -2,27 +2,27 @@ const db = require('./index.js');
 
 db.conn.sync().then(() => {
   db.models.Conclusions.update(
-    { conclusion: 'Natur pur-Ei' },
+    { conclusion: 'Du bist ein Natur pur-Ei' },
     { where: { id: 24 } }
   )
   db.models.Conclusions.update(
-    { conclusion: 'Kinder-Überraschungs-Ei' },
+    { conclusion: 'Du bist ein Kinder-Überraschungs-Ei' },
     { where: { id: 25 } }
   )
   db.models.Conclusions.update(
-    { conclusion: 'Too-much-opulentes-Ei' },
+    { conclusion: 'Du bist ein Too-much-opulentes-Ei' },
     { where: { id: 26 } }
   )
   db.models.Conclusions.update(
-    { conclusion: 'Leeres Nest' },
+    { conclusion: 'Du bist ein leeres Nest' },
     { where: { id: 27 } }
   )
   db.models.Conclusions.update(
-    { conclusion: 'Fabergé-Ei' },
+    { conclusion: 'Du bist ein Fabergé-Ei' },
     { where: { id: 28 } }
   )
   db.models.Conclusions.update(
-    { conclusion: 'Vorgefärbtes Supermarkt-Ei' },
+    { conclusion: 'Du bist ein vorgefärbtes Supermarkt-Ei' },
     { where: { id: 29 } }
   )
 
